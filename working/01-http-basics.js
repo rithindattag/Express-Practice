f91312905963e6c76fd90aed372fdaf8138abd2d
Rithindatta Gundu <rithindatta.g@gmail.com>
2023-06-06 17:14:42 +0530

    Calling navbar project from the server

--- working/01-http-basics.js
+++ working/01-http-basics.js
@@ -1,7 +1,10 @@
 const http = require("http");
 const { readFileSync } = require("fs");
 
-const homePage = readFileSync("../index.html");
+const homePage = readFileSync("../navbar-app/index.html");
+const homeStyles = readFileSync("../navbar-app/styles.css");
+const homeLogo = readFileSync("../navbar-app/logo.svg");
+const homeBrowser = readFileSync("../navbar-app/browser-app.js");
 
 const server = http.createServer((req, res) => {
   // console.log(req.method);
@@ -12,6 +15,26 @@ const server = http.createServer((req, res) => {
     res.writeHead(200, { "content-type": "text/html" });
     res.write(homePage);
     res.end();
+  }
+
+  // CSS Styles
+  else if (url === "/styles.css") {
+    res.writeHead(200, { "content-type": "text/css" });
+    res.write(homeStyles);
+    res.end();
+  }
+
+  // Logo
+  else if (url === "/logo.svg") {
+    res.writeHead(200, { "content-type": "image/svg+xml" });
+    res.write(homeLogo);
+    res.end();
+  }
+
+  // Browser JavaScript
+  else if (url === "/browser-app.js") {
+    res.writeHead(200, { "content-type": "text/javascript" });
+    res.write(homeBrowser);
   }
 
   // About Page
