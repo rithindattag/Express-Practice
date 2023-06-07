ec595f67944d7505f58570f96bac51be5db0ea0b
Rithindatta Gundu <rithindatta.g@gmail.com>
2023-06-07 19:11:00 +0530

    Used Route Params

--- app.js
+++ app.js
@@ -7,11 +7,26 @@ app.get("/", (req, res) => {
 });
 
 app.get("/api/products", (req, res) => {
-    const newProducts = products.map((products) => {
-        const { id, name, image } = products;
-        return { id, name, image };
-    })
+  const newProducts = products.map((product) => {
+    const { id, name, image } = product;
+    return { id, name, image };
+  });
   res.json(newProducts);
+});
+
+app.get("/api/products/:productID", (req, res) => {
+  const { productID } = req.params;
+  const singleProduct = products.find(
+    (product) => product.id === Number(productID)
+  );
+  if (!singleProduct) {
+    return res.status(404).send("Product doesnot exists!");
+  }
+  res.json(singleProduct);
+});
+
+app.get("/api/products/:productID/review/:reviewID", (req, res) => {
+  res.send("The product is good!");
 });
 
 app.listen(5000, () => {
