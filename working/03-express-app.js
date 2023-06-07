cc715eb5396d9ba3348e058cb5f1b29aab0ad51c
Rithindatta Gundu <rithindatta.g@gmail.com>
2023-06-07 12:28:58 +0530

    Added express-app.js file

---
+++ working/03-express-app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+app.use(express.static("../public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../navbar-app/index.html"));
+});
+
+app.listen(5000, () => {
+  console.log("Server listening at port 5000...");
+});
