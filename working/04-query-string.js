afa84b9ee2b28a8e90bd1d53e824e4408c29d349
Rithindatta Gundu <rithindatta.g@gmail.com>
2023-06-08 18:12:34 +0530

    Adding query-string file to working folder

---
+++ working/04-query-string.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const app = express();
+const { products } = require("./data");
+
+app.get("/", (req, res) => {
+  res.send('<h1>Hello there!</h1><a href="/api/products">Products</a>');
+});
+
+app.get("/api/products", (req, res) => {
+  const newProducts = products.map((product) => {
+    const { id, name, image } = product;
+    return { id, name, image };
+  });
+  res.json(newProducts);
+});
+
+app.get("/api/products/:productID", (req, res) => {
+  const { productID } = req.params;
+  const singleProduct = products.find(
+    (product) => product.id === Number(productID)
+  );
+  if (!singleProduct) {
+    return res.status(404).send("Product doesnot exists!");
+  }
+  res.json(singleProduct);
+});
+
+app.get("/api/products/:productID/review/:reviewID", (req, res) => {
+  res.send("The product is good!");
+});
+
+app.get('/api/v1/query', (req, res) => {
+  const { search, limit } = req.query
+  let sortedProducts = [...products]
+
+  if(search) {
+    sortedProducts = sortedProducts.filter((product) => {
+      return product.name.startsWith(search)
+    })
+  }
+
+  if(limit) {
+    sortedProducts = sortedProducts.slice(0, Number(limit))
+  }
+
+  if(sortedProducts < 1) {
+    return res.status(200).send("No products matched your search");
+  }
+  return res.status(200).json(sortedProducts)
+})
+
+app.listen(5000, () => {
+  console.log("Server listening at port 5000...");
+});
